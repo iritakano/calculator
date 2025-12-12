61685db8b3f9554d229f4d596ea48efd15241ca2
Iri Takano <iri2121@icloud.com>
2025-12-12 16:20:41 -0500

    change function so buttons displayed if digit or last pressed operator. Once = is clicked, takes digits before operator and stores in num1, and digits after operator and stores num2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function divide(num1, num2){
 }
 
 let num1;
-let operator;
+let operator = '';
 let num2;
 
 function operate(operator, num1, num2){
@@ -33,14 +33,55 @@ function operate(operator, num1, num2){
     }
 }
 
-button = document.querySelectorAll('button');
-display = document.querySelector('.display');
+let button = document.querySelectorAll('button');
+let display = document.querySelector('.display');
+
+let operators = ['+', '-', 'x', '÷'];
+let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+let numbers = [];
 
 button.forEach(button => {
     button.addEventListener('click', (e) => {
         let clicked = document.createElement('p');
         clicked.textContent = e.target.textContent;
-        display.appendChild(clicked);
+
+        if(digits.includes(e.target.textContent)){
+            numbers.push(e.target.textContent);
+            display.appendChild(clicked);
+        }
+
+        if(operators.includes(e.target.textContent)){
+            if(operator != ''){
+                numbers.pop();
+                display.removeChild(display.lastChild);
+            }
+
+            operator = e.target.textContent;
+            numbers.push(e.target.textContent);
+            display.appendChild(clicked);            
+        }
+
+        if(e.target.textContent == '='){
+            numbers.forEach(button => {
+                if(operators.includes(button)){
+                let operator_index = numbers.indexOf(button);
+
+                num1 = numbers.slice(0, operator_index).join('');
+                num2 = numbers.slice(operator_index + 1).join('');
+                if(num2 == ''){
+                    num2 = 0;
+                }
+
+                console.log(operator);
+                console.log("num11", num1);
+                console.log("num2", num2);
+                }
+            })
+        }
     })
 });
+
+
+
+
 
