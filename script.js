2a587b912a855972d8f37e52fbf8c86d0a7891e7
Iri Takano <iri2121@icloud.com>
2025-12-12 13:14:58 -0500

    create operate fxn that takes operator and two numbers and calls one of functions with those two numbers

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function divide(num1, num2){
 let num1;
 let operator;
 let num2;
+
+function operate(operator, num1, num2){
+    if(operator == '+'){
+        add(num1, num2);
+    }
+    else if(operator == '-'){
+        subtract(num1, num2);
+    }
+    else if(operator == '*'){
+        multiply(num1, num2);
+    }
+    else if(operator == '/'){
+        divide(num1, num2);
+    }
+}
